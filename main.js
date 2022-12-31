f077f76dc5e26079d4533648b62386a5ec74e2f6
DedlySpyder <dedlyspyder@gmail.com>
2022-12-31 16:36:32 -0500

    Fixed styling on main.js

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
 const https = require('https');
 const core = require('@actions/core');
 
-https.get('https://factorio.com/api/latest-releases', res => {
-    let data = [];
-    res.on('data', chunk => {
-        data.push(chunk);
-    });
-    res.on('end', () => {
-        const raw = Buffer.concat(data).toString()
-        const versions = JSON.parse(raw);
-        console.log(`Response from Factorio: ${raw}`)
+https.get('https://factorio.com/api/latest-releases', (res) => {
+  const data = [];
+  res.on('data', (chunk) => {
+    data.push(chunk);
+  });
+  res.on('end', () => {
+    const raw = Buffer.concat(data).toString();
+    const versions = JSON.parse(raw);
+    console.log(`Response from Factorio: ${raw}`);
 
-        const type = core.getInput('version_type')
-        core.setOutput('experimental', versions.experimental[type])
-        core.setOutput('stable', versions.stable[type])
-    });
-}).on('error', err => {
-    core.setFailed(err.message)
+    const type = core.getInput('version_type');
+    core.setOutput('experimental', versions.experimental[type]);
+    core.setOutput('stable', versions.stable[type]);
+  });
+}).on('error', (err) => {
+  core.setFailed(err.message);
 });
